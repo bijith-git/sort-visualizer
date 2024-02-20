ecc2d7977f3daf8e17c7b73c4976097219a9c9fc
Bijith <bijithpn@gmail.com>
2024-02-20 22:59:52 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/screen1.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
